142ddaebd0f1cfaaed94ea085ad5657c7a3c37a3
Omar_Jupiter <omarahmed9113@gmail.com>
2023-01-30 23:51:27 +0200

    Update d n js

--- Dockerized_nodejs_app/index.js
+++ Dockerized_nodejs_app/index.js
@@ -4,5 +4,5 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Hello, it\'s me, How are you Mr.Z3bla! </h1>'));
+app.get('/', (req, res) => res.send('<h1>Hello, it\'s me, How are you Mr.Z3bla!! </h1>'));
 app.listen(PORT, () => console.log('app is up and running on port: ${PORT}'))
