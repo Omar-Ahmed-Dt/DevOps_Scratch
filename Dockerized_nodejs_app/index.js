16517e7bf3b15efe8b200b922633384b0f20295f
Omar_Jupiter <omarahmed9113@gmail.com>
2023-02-02 23:49:14 +0200

    Delete index.js

--- Dockerized_nodejs_app/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-// init app 
-const PORT = 4000;
-const app = express();
-
-app.get('/', (req, res) => res.send('<h1>Hello, it\'s me, How are you Mr.Z3bla!! </h1>'));
-app.listen(PORT, () => console.log('app is up and running on port: ${PORT}'))
