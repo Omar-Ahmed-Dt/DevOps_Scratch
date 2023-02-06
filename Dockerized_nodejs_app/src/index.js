1e262258d434c3b98521fac77bdb81e06fb9c5d2
Omar_Jupiter <omarahmed9113@gmail.com>
2023-02-06 23:24:24 +0200

    Update nodejs

--- Dockerized_nodejs_app/src/index.js
+++ Dockerized_nodejs_app/src/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
 
 // init app 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Hello, it\'s me, How are you Mr.Z3bla! </h1>'));
+// connect db 
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+//const DB_HOST = '172.19.0.3'; 
+// or 
+const DB_HOST = 'mongo';
+
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+mongoose
+    .connect(URI)
+    .then(() => console.log('connect to db ...'))
+    .catch((err) => console.log('failed to connect to db: ', err));
+
+
+app.get('/', (req, res) => res.send('<h1>Hello, it\'s me, How are you Mr.Z3bla!! </h1>'));
 app.listen(PORT, () => console.log('app is up and running on port: ${PORT}'))
